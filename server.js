e04f323188a8f2292085eb14e0e6c51c992c4579
jwchan1996 <741755613@qq.com>
2021-01-30 00:29:34 +0800

    feat: express 服务器路由返回渲染后的 Vue 实例

--- server.js
+++ server.js
@@ -1,18 +1,41 @@
 const Vue = require('vue')
 const renderer = require('vue-server-renderer').createRenderer()
+const express = require('express')
+const server = express()
 
-const app = new Vue({
-  template: `
-    <div id="app">
-      <h1>{{ message }}</h1>
-    </div>
-  `,
-  data: {
-    message: '飘香豆腐'
-  }
+server.get('/', (req, res) => {
+  const app = new Vue({
+    template: `
+      <div id="app">
+        <h1>{{ message }}</h1>
+      </div>
+    `,
+    data: {
+      message: '飘香豆腐'
+    }
+  })
+  renderer.renderToString(app, (err, html) => {
+    if (err) {
+      return res.status(500).end('Internal Server Error')
+    }
+    res.setHeader('content-type', 'text/html; charset=utf8')
+    return res.end(`
+      <!DOCTYPE html>
+      <html lang="zh">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>vue-ssr</title>
+      </head>
+      <body>
+        ${html}
+      </body>
+      </html>
+    `)
+  })
 })
 
-renderer.renderToString(app, (err, html) => {
-  if (err) throw err
-  console.log(html)
+server.listen(3000, () => {
+  console.log('server listen on port 3000')
 })
