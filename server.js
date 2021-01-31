eb806bea0c35067e86c02155d09f572153330859
jwchan1996 <741755613@qq.com>
2021-01-31 21:55:59 +0800

    feat: 启动同构应用

--- server.js
+++ server.js
@@ -1,35 +1,21 @@
 const Vue = require('vue')
 const fs = require('fs')
-const renderer = require('vue-server-renderer').createRenderer({
-  template: fs.readFileSync('./index.template.html', 'utf8')
-})
 const express = require('express')
 const server = express()
 
+const clientManifest = require('./dist/vue-ssr-client-manifest.json')
+const serverBundle = require('./dist/vue-ssr-server-bundle.json')
+const template = fs.readFileSync('./index.template.html', 'utf8')
+
+const renderer = require('vue-server-renderer').createBundleRenderer(serverBundle, {
+  template,
+  clientManifest
+})
+
+server.use('/dist', express.static('./dist'))
+
 server.get('/', (req, res) => {
-  const app = new Vue({
-    template: `
-      <div id="app">
-        <h1>{{ message }}</h1>
-        <h2>客户端动态交互</h2>
-        <div>
-          <input v-model="message" />
-        </div>
-        <div>
-          <button @click="onClick">点击测试</button>
-        </div>
-      </div>
-    `,
-    data: {
-      message: '飘香豆腐'
-    },
-    methods: {
-      onClick() {
-        console.log('Hello World!')
-      }
-    }
-  })
-  renderer.renderToString(app, {
+  renderer.renderToString({
     title: 'vue-server-side-render',
     meta: `
       <meta name='vue-ssr' content='vue-server-side-render'>
