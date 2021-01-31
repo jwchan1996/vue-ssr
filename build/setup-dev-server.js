30178a4511581a9d824fd5f8bcc71ee49372b54f
jwchan1996 <741755613@qq.com>
2021-01-31 23:59:03 +0800

    feat: 提取处理模块

---
+++ build/setup-dev-server.js
@@ -0,0 +1,7 @@
+module.exports = (server, callback) => {
+  const onReady = new Promise()
+
+  // 监视构建 -> 更新 Renderer
+
+  return onReady
+}
--- server.js
+++ server.js
@@ -1,22 +1,38 @@
-const Vue = require('vue')
 const fs = require('fs')
 const express = require('express')
 const server = express()
-
-const clientManifest = require('./dist/vue-ssr-client-manifest.json')
-const serverBundle = require('./dist/vue-ssr-server-bundle.json')
-const template = fs.readFileSync('./index.template.html', 'utf8')
-
-// 通过读取打包后的 server-bundle.js 内容（entry--server.js 为入口文件的内容）加载得到 vue 实例
-// 然后将 vue 实例字符串注入到 template 中
-const renderer = require('vue-server-renderer').createBundleRenderer(serverBundle, {
-  template,
-  clientManifest
-})
+const setupDevServer = require('./build/setup-dev-server')
 
 server.use('/dist', express.static('./dist'))
 
-server.get('/', (req, res) => {
+const isProd = process.env.NODE_ENV === 'production'
+
+let render
+let onReady
+
+if (isProd) {
+  // 生产模式下基于构建后的 dist 文件
+  const clientManifest = require('./dist/vue-ssr-client-manifest.json')
+  const serverBundle = require('./dist/vue-ssr-server-bundle.json')
+  const template = fs.readFileSync('./index.template.html', 'utf8')
+
+  // 通过读取打包后的 server-bundle.js 内容（entry--server.js 为入口文件的内容）加载得到 vue 实例
+  // 然后将 vue 实例字符串注入到 template 中
+  renderer = require('vue-server-renderer').createBundleRenderer(serverBundle, {
+    template,
+    clientManifest
+  })
+} else {
+  // 开发模式 -> 监视打包构建 -> 重新生成 Renderer 渲染器
+  onReady = setupDevServer(server, (serverBundle, template, clientManifest) => {
+    renderer = require('vue-server-renderer').createBundleRenderer(serverBundle, {
+      template,
+      clientManifest
+    })
+  })
+}
+
+const render = (req, res) => {
   renderer.renderToString({
     title: 'vue-server-side-render',
     meta: `
@@ -29,7 +45,16 @@ server.get('/', (req, res) => {
     res.setHeader('content-type', 'text/html; charset=utf8')
     return res.end(html)
   })
-})
+}
+
+server.get('/', isProd
+  ? render
+  : async (req, res) => {
+    // 等待有了 Renderer 渲染器以后，才能调用 render 函数进行渲染
+    await onReady
+    render()
+  }
+)
 
 server.listen(3000, () => {
   console.log('server listen on port 3000')
