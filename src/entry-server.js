b8ff123d3169d731ba5597cd8e1548a0a3f00ce4
jwchan1996 <741755613@qq.com>
2021-02-21 15:18:50 +0800

    feat: 路由处理适配服务端入口

--- src/entry-server.js
+++ src/entry-server.js
@@ -3,10 +3,43 @@
  */
 import { createApp } from './app'
 
-export default context => {
-  const { app } = createApp()
+// export default context => {
+//   // 因为有可能会是异步路由钩子函数或组件，所以我们将返回一个 Promise，
+//   // 以便服务器能够等待所有的内容在渲染前，
+//   // 就已经准备就绪。
+//   return new Promise((resolve, reject) => {
+//     const { app, router } = createApp()
 
-  // 服务端路由处理、数据预取...
+//     // 设置服务器端 router 的位置
+//     router.push(context.url)
+
+//     // 等到 router 将可能的异步组件和钩子函数解析完
+//     router.onReady(() => {
+//       const matchedComponents = router.getMatchedComponents()
+//       // 匹配不到的路由，执行 reject 函数，并返回 404
+//       if (!matchedComponents.length) {
+//         return reject({ code: 404 })
+//       }
+//       // 注意：上面这一步由于 VueRouter 配置了通配符，匹配不到会自动加载 404 组件
+//       // 所以上面这一块带代码可以省略
+
+//       // Promise 应该 resolve 应用程序实例，以便它可以渲染
+//       resolve(app)
+//     }, reject)
+//   })
+// }
+
+export default async context => {
+  // 因为有可能会是异步路由钩子函数或组件，所以我们将返回一个 Promise，
+  // 以便服务器能够等待所有的内容在渲染前，
+  // 就已经准备就绪。
+  const { app, router } = createApp()
+
+  // 设置服务器端 router 的位置
+  router.push(context.url)
+
+  // 等到 router 将可能的异步组件和钩子函数解析完
+  await new Promise(router.onReady.bind(router))
 
   return app
 }
