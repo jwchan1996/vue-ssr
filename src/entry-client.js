0e54490aa26d15ec0d9ffad572dc3e0bad2d0c1d
jwchan1996 <741755613@qq.com>
2021-02-21 15:31:42 +0800

    feat: 路由处理适配客户端入口

--- src/entry-client.js
+++ src/entry-client.js
@@ -5,7 +5,10 @@ import { createApp } from './app'
 
 // 客户端特定引导逻辑……
 
-const { app } = createApp()
+const { app, router } = createApp()
 
-// 这里假定 App.vue 模板中根元素具有 `id="app"`
-app.$mount('#app')
+// 等待路由初始化完成
+router.onReady(() => {
+  // 这里假定 App.vue 模板中根元素具有 `id="app"`
+  app.$mount('#app')
+})
